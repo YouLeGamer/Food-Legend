0629b5fe92849f91df130f99f5fc715fe0496972
firefood <DoukiYanis75@gmail.com>
2023-08-19 16:07:10 +0100

    cool update

--- Food's Legends/scripts/main_menu.gd
+++ Food's Legends/scripts/main_menu.gd
@@ -1,10 +1,6 @@
 extends Control
 
 
-func _ready():
-	$MarginContainer/VBoxContainer/Play.set_pos(Vector2(50, 50))
-
-
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
